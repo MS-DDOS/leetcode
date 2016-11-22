5878e5aa54ec32bd599f871b24ebffc0a0f73b1d
Tyler Springer <mspringer@smu.edu>
2016-11-22 14:21:18 -0800

    Added 146.) LRU Cache...using standard python dictionary and list

---
+++ algorithms/146_LRU_Cache/LRU_Cache.py
@@ -0,0 +1,41 @@
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.cache = {}
+        self.LRU = []
+
+    def get(self, key):
+        """
+        :rtype: int
+        """
+        if key not in self.cache:
+            return -1
+        self.LRU.remove(key)
+        self.LRU.insert(0, key)
+        return self.cache[key]
+
+    def set(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: nothing
+        """
+        if key in self.cache:
+            self.LRU.remove(key)
+        else:
+            if len(self.cache) == self.capacity:
+                del self.cache[self.LRU.pop()]
+        self.LRU.insert(0,key)
+        self.cache[key] = value
+
+if __name__ == "__main__":
+    x = LRUCache(5)
+    x.set(2,1)
+    x.set(4,1)
+    x.set(5,2)
+    x.set(3,4)
+    print x.get(2)
