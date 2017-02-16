389395360c127e7e25a5d6854628c08a280385ad
Tyler Springer <mspringer@smu.edu>
2017-02-16 13:54:13 -0800

    Added a C++ implementation of reversing a linked list

---
+++ algorithms/206_reverse_linked_list/rev_linked_list.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+struct node{
+	int val;
+	struct node* next;
+};
+
+void push(struct node **, int);
+void print_list(struct node *);
+void reverse_list(struct node **);
+
+int main(int argc, char ** argv){
+	node * list = NULL;
+	push(&list, 4);
+	push(&list, 3);
+	push(&list, 2);
+	push(&list, 1);
+
+	print_list(list);
+	reverse_list(&list);
+	print_list(list);
+
+}
+
+void reverse_list(struct node ** head_ref){
+	struct node * previous = NULL;
+	struct node * curr = *head_ref;
+	while(curr != NULL){
+		struct node * next = curr->next;
+		curr->next = previous;
+		previous = curr;
+		curr = next;
+	}
+	*head_ref = previous;
+}
+
+void push(struct node ** head_ref, int data) {
+	struct node * new_node = (struct node*)malloc(sizeof(struct node));
+	
+	new_node->val = data;
+	new_node->next = *head_ref; //deref since its a pointer
+	*head_ref = new_node;
+}
+
+void print_list(struct node * head_ref){
+	struct node * ref = head_ref;
+	while(ref != NULL){
+		std::cout << ref->val << std::endl;
+		ref = ref->next;
+	}
+}
